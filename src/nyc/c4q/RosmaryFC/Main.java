5adaab3c6c76d1d20b4c74aef79e34da9fee7b46
RosmaryFC <Rosmaryfc@hotmail.com>
2015-03-01 17:41:04 -0500

    initial commit

---
+++ src/nyc/c4q/RosmaryFC/Main.java
@@ -0,0 +1,9 @@
+package nyc.c4q.RosmaryFC;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+        System.out.println("Hi");
+    }
+}
---
+++ src/nyc/c4q/RosmaryFC/Temperature.java
@@ -0,0 +1,11 @@
+package nyc.c4q.RosmaryFC;
+
+/**
+ * Created by c4q-rosmary on 3/1/15.
+ */
+public class Temperature {
+    public static void main( String[] args ){
+        System.out.println("temperature: " + (((-4*9)/5) + 32) + "\u00b0F");
+
+    }
+}
