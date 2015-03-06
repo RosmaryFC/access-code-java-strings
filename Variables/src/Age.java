4072460ef1ce5203596c69001b11abb15f1b3271
RosmaryFC <Rosmaryfc@hotmail.com>
2015-03-05 19:13:09 -0500

    variables

---
+++ Variables/src/Age.java
@@ -0,0 +1,11 @@
+/**
+ * Created by c4q-rosmary on 3/3/15.
+ */
+public class Age {
+    public static void main (String[] args){
+
+        int YourAge = 22;
+
+        System.out.println("Today I am " + YourAge + " years old. In 5 Years I will be " + (YourAge + 5) + " years old.");
+    }
+}
---
+++ Variables/src/Vars.java
@@ -0,0 +1,24 @@
+/**
+ * Created by c4q-rosmary on 3/3/15.
+ */
+public class Vars {
+    public static void main(String[] args) {
+        int favNum = 9;
+        System.out.println("I like the number " + favNum);
+
+        //
+        //
+        //
+        //
+        //
+
+        System.out.println(favNum + " is a prime number");
+
+        double favNum1 = 7;
+        System.out.print("I like the number " + favNum1);
+
+        char letter = 'a';
+        boolean more = 2 < 1;
+
+    }
+}
